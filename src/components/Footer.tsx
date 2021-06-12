83225b4ca67a1667f565bf2d6fa8bd4148ac102c
Alex Spahos <alexspahos@gmail.com>
2021-06-12 14:40:09 +0300

    complete class to function component changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,7 @@
-export const Footer = () => {
-    return (
-        <footer className="footer">
-            <div className="content">
-                <p>Created by <a href="https://github.com/alexspahos">Alex Spahos</a> with <a href="https://bulma.io/" target="_blank">Bulma CSS framework</a> and <a href="https://www.coingecko.com/en/api" target="_blank">CoinGecko API</a></p>
-            </div>
-        </footer>
-    );
-}
+export const Footer = () => (
+    <footer className="footer">
+        <div className="content">
+            <p>Created by <a href="https://github.com/alexspahos">Alex Spahos</a> with <a href="https://bulma.io/" target="_blank">Bulma CSS framework</a> and <a href="https://www.coingecko.com/en/api" target="_blank">CoinGecko API</a></p>
+        </div>
+    </footer>
+);
